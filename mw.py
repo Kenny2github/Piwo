3e1905779e5f8f95a6570861cfe0b4afdfed39f1
Ken <kenny2minecraft@gmail.com>
2020-10-19 18:43:14 +0800

    escape moved from cgi to html

--- mw.py
+++ mw.py
@@ -1,6 +1,6 @@
 from sys import argv
 from os import environ as env
-from cgi import escape as hsc
+from html import escape as hsc
 
 MW_ROOT = env.get('MW_ROOT', None)
 GRAM_NAME = env.get('MW_GRAM_NAME', None)
